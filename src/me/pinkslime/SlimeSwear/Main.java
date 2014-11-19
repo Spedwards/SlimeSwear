48d9ecddf70273789f281f60ac21308f94991916
Liam <improbablepuzzle@gmail.com>
2014-11-19 23:32:27 +1100

    Sponge Plugin

---
+++ src/me/pinkslime/SlimeSwear/Main.java
@@ -0,0 +1,52 @@
+package me.pinkslime.SlimeSwear;
+
+import org.spongepowered.api.entity.Player;
+import org.spongepowered.api.event.Subscribe;
+import org.spongepowered.api.event.player.PlayerChatEvent;
+import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.util.command.CommandSource;
+
+@Plugin(id="SlimeSwear", name="SlimeSwear", version="1.0.0-SNAPSHOT")
+public class Main {
+	
+	@Subscribe
+	public void chatEvent(PlayerChatEvent e) {
+		
+		String msg = e.getMessage();
+		Player player = e.getPlayer();
+		CommandSource src = e.getSource();
+		
+		msg = msg.replaceAll("(?i)arsehole|asshole", "Elephant and Castle");
+		msg = msg.replaceAll("(?i)arse|ass", "Bottle and Glass");
+		msg = msg.replaceAll("(?i)balls", "Niagra Falls");
+		msg = msg.replaceAll("(?i)bitch", "Miss Fitch");
+		msg = msg.replaceAll("(?i)bum", "Kingdom Come");
+		msg = msg.replaceAll("(?i)cock", "Brighton Rock");
+		msg = msg.replaceAll("(?i)crap", "Von Trappe");
+		msg = msg.replaceAll("(?i)cum", "Bubble Gum");
+		msg = msg.replaceAll("(?i)cunt", "Berkeley Hunt");
+		msg = msg.replaceAll("(?i)dump", "Donald Trump");
+		msg = msg.replaceAll("(?i)fanny", "Jack and Danny");
+		msg = msg.replaceAll("(?i)fart", "Raspberry Tart");
+		msg = msg.replaceAll("(?i)Fucker", "Feather Plucker");
+		msg = msg.replaceAll("(?i)fuck", "Friar Tuck");
+		msg = msg.replaceAll("(?i)horn", "Popcorn");
+		msg = msg.replaceAll("(?i)pissed", "Oliver Twist");
+		msg = msg.replaceAll("(?i)piss", "Hit and Miss");
+		msg = msg.replaceAll("(?i)prick", "Hampton Wick");
+		msg = msg.replaceAll("(?i)pube", "Rubik's Cube");
+		msg = msg.replaceAll("(?i)shitter", "Gary Glitter");
+		msg = msg.replaceAll("(?i)shite", "Turkish Delight");
+		msg = msg.replaceAll("(?i)shits", "Two Bob Bits");
+		msg = msg.replaceAll("(?i)shit", "Brad Pitt");
+		msg = msg.replaceAll("(?i)spunk", "Harry Monk");
+		msg = msg.replaceAll("(?i)tits", "Brace and Bits");
+		msg = msg.replaceAll("(?i)titty", "Bristol City");
+		msg = msg.replaceAll("(?i)Turd", "Douglas Hurd");
+		msg = msg.replaceAll("(?i)wanker", "Merchant Banker");
+		msg = msg.replaceAll("(?i)wank", "Sherman Tank");
+				
+		e.setMessage(msg);
+	}
+	
+}
